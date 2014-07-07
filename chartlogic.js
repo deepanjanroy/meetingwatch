080f04a1c70f69afd0ceaa7a4b6c4b2ffd69638e
edeeroy <deepanjan.roy@ericsson.com>
2014-07-07 03:05:51 -0400

    lines, better dates, and dummy stopwatch
    Using a continuously updating svg d element.
    Switched out date formatting to d3.time.format > parse.
    The new dataPoint is now real time measurement, synced with the stopwatch.

--- chartlogic.js
+++ chartlogic.js
@@ -15,6 +15,7 @@ var x = d3.time.scale()
 
 var xAxis = d3.svg.axis()
     .scale(x)
+    .ticks(7) // Move this somewhere else
     .orient("bottom");
 
 var yAxis = d3.svg.axis()
@@ -28,6 +29,8 @@ var chart = d3.select("body").append("svg")
   .append("g")
     .attr("transform", "translate(" + margin.left + "," + margin.top + ")");
 
+var date_parser = d3.time.format("%Y-%m-%d");
+var new_data = {date: date_parser.parse("2014-07-29"), value: 0};
 
 // Async call to data.tsv
 d3.tsv("data.tsv", function(error, data){
@@ -38,12 +41,38 @@ d3.tsv("data.tsv", function(error, data){
   }
 
   data.forEach(function(d) {
-    d.date = +d.date;
     d.value = +d.value;
+    d.date = new date_parser.parse(d.date);
   });
 
+  var lambdaMax = function(arr, lambda) {
+    if (arr.length === 0 || arr === undefined || arr === null) {
+      throw "Invalid argument";
+    }
+
+    if (lambda === null || lambda === undefined) {
+      lambda = function (key) { return key; };
+    }
+
+    var max_index = 0;
+    var cur_max = lambda(arr[max_index]);
+
+    arr.forEach(function (val, i) {
+      if (lambda(val) > cur_max) {
+        max_index = i;
+      }
+    })
+
+    return arr[max_index];
+  }
+
   // Setting domain
-  x.domain([20, d3.max(data, function(d) { return d.date; })]);
+  var max_date_data = lambdaMax(data, function(d) {  return new Date(d.date).getTime(); });
+  var max_date = new Date(max_date_data.date);
+  var lower_date = new Date(max_date.getUTCFullYear(), max_date.getUTCMonth(), max_date.getUTCDate() - 5);
+  var upper_date = new Date(max_date.getUTCFullYear(), max_date.getUTCMonth(), max_date.getUTCDate() + 1);
+
+  x.domain([lower_date, upper_date]);
   y.domain([0, d3.max(data, function(d) { return d.value; })]);
 
   // Adding axes
@@ -64,10 +93,11 @@ d3.tsv("data.tsv", function(error, data){
       .attr("r", "4")
       .attr("fill", "steelblue");
 
+
   chart.selectAll(".newPoint")
-      .data([{date: 26, value: 0}])
+      .data([new_data])
     .enter().append("circle")
-      .attr("class", "newPoint")
+      .attr("class", "newPoint dataPoint")
       .attr("id", "newPointId")
       .attr("cx", function(d) { return x(d.date);})
       .attr("cy", function(d) { return y(d.value);})
@@ -75,12 +105,19 @@ d3.tsv("data.tsv", function(error, data){
       // .attr("fill", "steelblue");
 
 
-  var motion_interval = 10; //ms
-  setInterval(function() {
-    var elm = document.getElementById("newPointId");
-    var new_cy = y(y.invert(elm.getAttribute("cy")) + motion_interval / 1000);
-    elm.setAttribute("cy", new_cy);
-  }, motion_interval);
+  var lineFunction = d3.svg.line()
+                          .x(function(d) { return x(new Date(d.date)); })
+                          .y(function(d) { return y(d.value); })
+                          .interpolate("linear");
+
+  chart.insert("path", ":first-child")
+        .attr("id", "thePath")
+        .attr("d", lineFunction(data.concat([new_data])))
+        .attr("stroke", "steelblue")
+       .attr("stroke-width", 2)
+       .attr("fill", "none");
+
+
 
   var colors = ["steelblue", "red"];
   var radius = ["4", "6"];
@@ -100,5 +137,45 @@ d3.tsv("data.tsv", function(error, data){
   }
 
   setInterval(transy, 500);
+
+
+  // Timer stuff
+
+  firstTime = new Date().getTime() / 1000;
+
+  var deepRound = function (num, decimalPlaces) {
+    if (decimalPlaces === undefined) {
+      decimalPlaces = 2;
+    }
+    var factor = Math.pow(10, decimalPlaces);
+    num = num * factor;
+    num = Math.round(num);
+    num = num / factor;
+
+    return num;
+  }
+
+  var update_new_point = function(seconds) {
+    var elm = document.getElementById("newPointId");
+    if (elm === null) {
+      return;
+    }
+
+    new_data.value = seconds;
+    elm.setAttribute("cy", y(new_data.value));
+
+    d3.select("#thePath")
+        .attr("d", lineFunction(data.concat(new_data)));
+
+
+  };
+
+  setInterval(function () {
+    var cur_time = new Date().getTime() / 1000;
+    var time_passed = deepRound(cur_time - firstTime);
+
+    document.getElementById("timerId").textContent = time_passed;
+    update_new_point(time_passed);
+  }, 10);
 
 });
